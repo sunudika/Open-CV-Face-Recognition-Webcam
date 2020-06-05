dd1f9fc54a11f9159f5e9705cf7ae779c087454d
Sunu Ilham Pradika <sunudika@gmail.com>
2020-06-05 10:05:46 +0700

    Add: main source code

---
+++ 02_face_training.py
@@ -0,0 +1,67 @@
+''''
+Training Multiple Faces stored on a DataBase:
+	==> Each face should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> LBPH computed model will be saved on trainer/ directory. (if it does not exist, pls create one)
+	==> for using PIL, install pillow library with "pip install pillow"
+
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18   
+
+'''
+
+import cv2
+import numpy as np
+import imutils
+#from PIL import Image
+import os
+
+# Path for face image database
+path = 'dataset'
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+# function to get the images and label data
+def getImagesAndLabels(path):
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    faceSamples=[]
+    ids = []
+
+    for imagePath in imagePaths:
+        img = cv2.imread(imagePath, 0) # get h,w
+        #PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+
+        (h, w) = img.shape[:2]
+        center = (w / 2, h / 2)
+
+        # rotate the image by 180 degrees
+        for angle in np.arange(0, 360, 5):
+            M = cv2.getRotationMatrix2D(center, angle, 1.0)
+            rotated = cv2.warpAffine(img, M, (w, h))
+            cv2.imshow("rotated", rotated)
+            cv2.waitKey(0)
+
+            img_numpy = np.array(rotated,'uint8')
+
+            id = int(os.path.split(imagePath)[-1].split(".")[1])
+            faces = detector.detectMultiScale(img_numpy)
+
+            for (x,y,w,h) in faces:
+                faceSamples.append(img_numpy[y:y+h,x:x+w])
+                ids.append(id)
+            
+            
+
+    return faceSamples,ids
+
+print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+
+# Save the model into trainer/trainer.yml
+recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+# Print the numer of faces trained and end program
+print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
